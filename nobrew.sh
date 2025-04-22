8516f1ccfddbb018b62130707f6534d03986a85b
Ming Li <80295234+minglisyr@users.noreply.github.com>
2025-04-22 16:27:21 -0600

    Update nobrew.sh

--- nobrew.sh
+++ nobrew.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade
-sudo apt-get install build-essential unzip
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install build-essential unzip -y
 
 
 # Oh-My-Posh installation
 curl -k https://ohmyposh.dev/install.sh | bash
 echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
 echo 'eval "$(oh-my-posh init bash --config $HOME/.cache/oh-my-posh/themes/microverse-power.omp.json)"' >> ~/.bashrc
-oh-my-posh font install meslo
 source ~/.bashrc
+oh-my-posh font install meslo
+
 
 # Tools installation
-sudo apt install tree
+sudo apt install tree python3-pip fzf -y
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org uv --break-system-packages
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org ruff --break-system-packages
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pyright --break-system-packages
 
 sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
 sudo apt update
 sudo apt install fastfetch
 
-
-uv fzf pyright fastfetch ruff
-
 # neovim installation
-brew install neovim
+sudo apt install neovim -y
+git config --global http.sslVerify false
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 nvim
@@ -29,6 +31,8 @@ nvim
 cd $HOME
 uv venv
 source .venv/bin/activate
+
+git config --global http.sslVerify true
 uv pip install vtk matplotlib numpy pandas
 
 cd ~/.local/share/nvim/lazy
