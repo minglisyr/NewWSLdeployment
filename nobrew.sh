68ec27979d46327e3426cafcde3bf2db7ceb76a9
Ming Li <80295234+minglisyr@users.noreply.github.com>
2025-04-23 14:26:24 -0600

    Update nobrew.sh

--- nobrew.sh
+++ nobrew.sh
@@ -8,8 +8,13 @@ curl -k https://ohmyposh.dev/install.sh | bash
 echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
 echo 'eval "$(oh-my-posh init bash --config $HOME/.cache/oh-my-posh/themes/microverse-power.omp.json)"' >> ~/.bashrc
 source ~/.bashrc
-oh-my-posh font install meslo
 
+sudo curl -kL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf --output /usr/local/share/fonts/MesloLGS-NF-Regular.ttf
+sudo curl -kL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf --output /usr/local/share/fonts/MesloLGS-NF-Bold.ttf
+sudo curl -kL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf --output /usr/local/share/fonts/MesloLGS-NF-Italic.ttf
+sudo curl -kL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf --output /usr/local/share/fonts/MesloLGS-NF-Bold-Italic.ttf
+fc-cache -fv
+fc-list | grep MesloLGS
 
 # Tools installation
 sudo apt install tree python3-pip fzf -y
@@ -20,11 +25,16 @@ pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pyrigh
 source build fastfetch >> https://github.com/fastfetch-cli/fastfetch/wiki/Building
 
 # neovim installation
-sudo apt install neovim -y
+curl -kLO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
 git config --global http.sslVerify false
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 nvim
+
+find . -mindepth 1 -maxdepth 1 -type d -exec git -C {} pull origin main \;
 
 cd $HOME
 uv venv
